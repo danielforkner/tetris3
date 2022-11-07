3ba81b02ffce0203b7d9c90138a72f71d5876c5a
danielforkner <daniel.forkner@gmail.com>
2022-11-07 14:27:28 -0500

    demo 3 code

--- script.js
+++ script.js
@@ -1,193 +1,77 @@
-// Game State and Classes
+// GAME STATE
 let game = {
-  running: false,
-  speed: 250,
-  width: 15,
-  height: 30,
-  score: 0,
-  board: [],
-  currentPiece: false,
-  nextPiece: [
+  playing: false,
+  timer: 0,
+  currentPiece: [
     [1, 1],
     [1, 1],
   ],
+  positionY: 0,
 };
 
-let shapes = [
-  [
-    [1, 1],
-    [1, 1],
-  ],
-  [
-    [0, 1],
-    [0, 1],
-    [0, 1],
-    [1, 1],
-  ],
-  [
-    [1, 0],
-    [1, 0],
-    [1, 0],
-    [1, 1],
-  ],
-  [
-    [0, 1, 0],
-    [1, 1, 1],
-  ],
-];
-
 // DOM Elements
-let table = document.getElementById('board');
-let boardRows = board.children;
-let playBtn = document.getElementById('playBtn');
-playBtn.addEventListener('click', () => {
-  game.running = !game.running;
-  if (game.running) {
-    playBtn.innerText = 'pause';
+let playBtn = document.getElementById('play');
+let timer = document.getElementById('timer');
+let table = document.getElementById('table');
+
+playBtn.addEventListener('click', function () {
+  game.playing = !game.playing;
+  if (game.playing) {
+    playBtn.innerText = 'Pause';
   } else {
-    playBtn.innerText = 'Play';
+    playBtn.innerText = 'Start';
   }
 });
 
-// Create Game Boards
-for (let i = 0; i < game.height; i++) {
-  game.board.push([]);
+// Game Board
+for (let i = 0; i < 30; i++) {
   let row = document.createElement('tr');
-  for (let j = 0; j < game.width; j++) {
-    game.board[i].push(0);
+  table.appendChild(row);
+  for (let j = 0; j < 15; j++) {
     let cell = document.createElement('td');
-    cell.classList.add('cell');
+    cell.id = j + '-' + i;
     row.appendChild(cell);
   }
-  table.appendChild(row);
 }
-
-window.addEventListener('keydown', (e) => {
-  console.log(e);
-  let piece = game.currentPiece;
-  switch (e.key) {
-    case 'a' || 'ArrowRight':
-      let leftPosition = piece.position.x;
-      if (leftPosition <= 0) {
-        break;
-      }
-      erasePiece();
-      piece.position.x--;
-      drawPiece();
-      break;
-    case 'd' || 'ArrowLeft':
-      let rightPosition = piece.position.x + piece.shape[0].length;
-      if (rightPosition >= game.board[0].length) {
-        break;
-      }
-      erasePiece();
-      piece.position.x++;
-      drawPiece();
-      break;
-    case 's' || 'ArrowDown':
-      break;
-  }
-});
-
-// Tick
-setInterval(() => {
-  if (game.running) {
-    if (!game.currentPiece) {
-      selectPiece();
-    }
-    erasePiece();
-    useGravity();
-    drawPiece();
-    if (checkBelow() && checkTop()) {
-      console.log('you lose');
-      game.running = false;
-    } else if (checkBelow()) {
-      selectPiece();
-    }
-  }
-}, game.speed);
 
 // Game Functions
-function selectPiece() {
-  let colors = ['red', 'blue', 'green'];
-  game.currentPiece = {
-    shape: game.nextPiece,
-    position: { x: Math.floor(game.width / 2) - 1, y: -1 },
-    color: colors[Math.floor(Math.random() * 3)],
-  };
-  game.nextPiece = shapes[Math.floor(Math.random() * shapes.length)];
-}
-
-function erasePiece() {
-  let piece = game.currentPiece;
-  let x = piece.position.x;
-  let y = piece.position.y;
-  let counter = 0;
-  for (let i = piece.shape.length - 1; i >= 0; i--) {
-    if (y - counter < 0) {
-      break;
-    }
-    let tableRow = table.children[y - counter];
-    for (let j = 0; j < piece.shape[i].length; j++) {
-      let tableCell = tableRow.children[j + x];
-      if (piece.shape[i][j]) {
-        tableCell.classList.remove(piece.color);
-        game.board[y - counter][j + x] = 0;
-      }
-    }
-    counter++;
+function advanceTime() {
+  if (game.playing === false) {
+    return;
   }
+  timer.innerText = ++game.timer;
 }
 
 function drawPiece() {
   let piece = game.currentPiece;
-  let x = piece.position.x;
-  let y = piece.position.y;
-  let counter = 0;
-  for (let i = piece.shape.length - 1; i >= 0; i--) {
-    if (y - counter < 0) {
-      break;
+  for (let i = 0; i < piece.length; i++) {
+    for (let j = 0; j < piece[i].length; j++) {
+      document
+        .getElementById(j + '-' + (i + game.positionY))
+        .classList.add('red');
     }
-    let tableRow = table.children[y - counter];
-    for (let j = 0; j < piece.shape[i].length; j++) {
-      let tableCell = tableRow.children[j + x];
-      if (piece.shape[i][j]) {
-        tableCell.classList.add(piece.color);
-        game.board[y - counter][j + x] = 1;
-      }
+  }
+}
+
+function invokeGravity() {
+  game.positionY++;
+}
+
+function removePiece() {
+  let piece = game.currentPiece;
+  for (let i = 0; i < piece.length; i++) {
+    for (let j = 0; j < piece[i].length; j++) {
+      let cell = document.getElementById(j + '-' + (i + game.positionY));
+      cell.classList.remove('red');
     }
-    counter++;
   }
 }
 
-function useGravity() {
-  let piece = game.currentPiece;
-  piece.position.y++;
-}
-
-function checkBelow() {
-  let piece = game.currentPiece;
-  let bottomRow = piece.shape[piece.shape.length - 1];
-  let x = piece.position.x;
-  let y = piece.position.y;
-  if (y >= game.board.length - 1) {
-    return true;
-  }
-  if (bottomRow[0] && game.board[y + 1][x]) {
-    return true;
-  }
-  if (bottomRow[1] && game.board[y + 1][x + 1]) {
-    return true;
-  }
-  return false;
-}
-
-function checkTop() {
-  let piece = game.currentPiece;
-  let y = piece.position.y;
-  // check if the top row of the piece is above the ceiling
-  if (y - piece.shape.length < 0) {
-    return true;
-  }
-  return false;
-}
+// Tick
+setInterval(function () {
+  if (!game.playing) return;
+  advanceTime();
+  removePiece();
+  invokeGravity();
+  drawPiece();
+}, 1000);
